e7771fde33132502e18168013a799f5550139616
BugHunter-X <askzenai@gmail.com>
2024-12-15 20:58:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,25 @@
+```javascript
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <MyComponent/>
+    </div>
+  );
+}
+
+// components/MyComponent.js
+export default function MyComponent() {
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div>Count: {count}</div>;
+}```
